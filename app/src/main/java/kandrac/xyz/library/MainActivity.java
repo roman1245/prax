485f3ae2c1712b4e336307cf44fc2899452ce933
Kandrac <Jan.Kandrac@firstdata.sk>
2015-08-10 16:05:42 +0200

    Zxing minimal added

--- app/src/main/java/kandrac/xyz/library/MainActivity.java
+++ app/src/main/java/kandrac/xyz/library/MainActivity.java
@@ -1,7 +1,13 @@
 package kandrac.xyz.library;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (scanResult != null) {
+            Toast.makeText(this, scanResult.getContents(), Toast.LENGTH_LONG).show();
+        }
     }
 }
