442b8759a55fd9500d067a5535a1837384015512
Kandrac <kandrac.jan@gmail.com>
2015-11-09 09:08:04 +0100

    Removed menus from author and publisher detail

--- app/src/main/java/kandrac/xyz/library/PublisherDetailActivity.java
+++ app/src/main/java/kandrac/xyz/library/PublisherDetailActivity.java
@@ -1,6 +1,5 @@
 package kandrac.xyz.library;
 
-import android.content.Intent;
 import android.database.Cursor;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
@@ -11,7 +10,6 @@ import android.support.v4.content.Loader;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 
@@ -79,25 +77,10 @@ public class PublisherDetailActivity extends AppCompatActivity implements Loader
 
     }
 
-    // ToolBar option menu
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.book_detail_menu, menu);
-        return true;
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         switch (id) {
-            case R.id.action_edit:
-                Intent intent = new Intent(this, EditBookActivity.class);
-                startActivity(intent);
-                return true;
-            case R.id.action_delete:
-                getContentResolver().delete(Contract.Publishers.CONTENT_URI, Contract.Publishers.PUBLISHER_ID + " = ?", new String[]{Long.toString(mPublisherId)});
-                finish();
-                return true;
             case android.R.id.home:
                 finish();
         }
