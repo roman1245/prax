26d987b4edda23bb23e162ee276f4980aa86ce4a
vizghar <kandrac.jan@gmail.com>
2015-11-17 20:12:10 +0100

    Book share button introduced
    Now the share button is enabled in Book Detail screen.
    It opens contact selection and searches for user name and
    its email.
    
    In future store the ID of selected person to database

--- app/src/main/java/kandrac/xyz/library/BookDetailActivity.java
+++ app/src/main/java/kandrac/xyz/library/BookDetailActivity.java
@@ -1,10 +1,17 @@
 package kandrac.xyz.library;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
@@ -14,6 +21,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -31,8 +39,18 @@ import kandrac.xyz.library.model.obj.Book;
 public class BookDetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     public static final String EXTRA_BOOK_ID = "book_id_extra";
+
+    static final int LOADER_BOOK = 1;
+    static final int LOADER_CONTACT = 2;
+
+    static final int PICK_CONTACT_PERMISSION = 1;
+    static final int PICK_CONTACT_ACTION = 1;
+
+    Uri contactUri;
+
     private Long mBookId;
     private BookDetailBinding binding;
+
 
     @Bind(R.id.toolbar)
     Toolbar toolbar;
@@ -61,36 +79,61 @@ public class BookDetailActivity extends AppCompatActivity implements LoaderManag
         }
 
         mBookId = getIntent().getExtras().getLong(EXTRA_BOOK_ID);
-        getSupportLoaderManager().initLoader(1, null, this);
+        getSupportLoaderManager().initLoader(LOADER_BOOK, null, this);
     }
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return new CursorLoader(this, Contract.Books.buildBookWithAuthorUri(mBookId), null, null, null, null);
+        switch (id) {
+            case LOADER_BOOK:
+                return new CursorLoader(this, Contract.Books.buildBookWithAuthorUri(mBookId), null, null, null, null);
+            case LOADER_CONTACT:
+                String contactId = contactUri.getLastPathSegment();
+                return new CursorLoader(
+                        this,
+                        ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+                        new String[]{ContactsContract.Data.DISPLAY_NAME, ContactsContract.CommonDataKinds.Email.DATA1},
+                        ContactsContract.CommonDataKinds.Email.CONTACT_ID + "=?",
+                        new String[]{contactId},
+                        null);
+        }
+        return null;
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        if (data.getCount() == 1) {
-            Book book = new Book(data);
-            binding.setBook(book);
-            collapsingToolbarLayout.setTitle(book.title);
+        int id = loader.getId();
+        switch (id) {
+            case LOADER_BOOK:
+                if (data.getCount() == 1) {
+                    Book book = new Book(data);
+                    binding.setBook(book);
+                    collapsingToolbarLayout.setTitle(book.title);
 
-            if (book.imageFilePath == null) {
-                return;
+                    if (book.imageFilePath == null) {
+                        return;
+                    }
+
+                    File file = new File(book.imageFilePath);
+
+                    if (!file.exists()) {
+                        return;
+                    }
+
+                    Picasso.with(this)
+                            .load(file)
+                            .resize(cover.getMeasuredWidth(), cover.getMeasuredHeight())
+                            .centerInside()
+                            .into(cover);
+                }
+                break;
+            case LOADER_CONTACT: {
+                if (data.moveToFirst()) {
+                    do {
+                        Toast.makeText(this, data.getString(0) + "," + data.getString(1), Toast.LENGTH_SHORT).show();
+                    } while (data.moveToNext());
+                }
             }
-
-            File file = new File(book.imageFilePath);
-
-            if (!file.exists()) {
-                return;
-            }
-
-            Picasso.with(this)
-                    .load(file)
-                    .resize(cover.getMeasuredWidth(), cover.getMeasuredHeight())
-                    .centerInside()
-                    .into(cover);
         }
     }
 
@@ -110,18 +153,66 @@ public class BookDetailActivity extends AppCompatActivity implements LoaderManag
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         switch (id) {
-            case R.id.action_edit:
+            case R.id.action_edit: {
                 Intent intent = new Intent(this, EditBookActivity.class);
                 intent.putExtra(EditBookActivity.EXTRA_BOOK_ID, mBookId);
                 startActivity(intent);
                 return true;
-            case R.id.action_delete:
+            }
+            case R.id.action_borrow: {
+                int readContactPermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
+                if (readContactPermission == PackageManager.PERMISSION_GRANTED) {
+                    searchContact();
+                } else {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, PICK_CONTACT_PERMISSION);
+                }
+                return true;
+            }
+            case R.id.action_delete: {
                 getContentResolver().delete(Contract.Books.buildBookUri(mBookId), null, null);
                 finish();
                 return true;
-            case android.R.id.home:
+            }
+            case android.R.id.home: {
                 finish();
+            }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PICK_CONTACT_PERMISSION: {
+
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    searchContact();
+                }
+                break;
+            }
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void searchContact() {
+        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(intent, PICK_CONTACT_ACTION);
+    }
+
+    @Override
+    public void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+
+        switch (reqCode) {
+            case (PICK_CONTACT_ACTION): {
+                if (resultCode == Activity.RESULT_OK) {
+                    contactUri = data.getData();
+                    getSupportLoaderManager().initLoader(LOADER_CONTACT, null, this);
+                }
+            }
+        }
     }
 }
