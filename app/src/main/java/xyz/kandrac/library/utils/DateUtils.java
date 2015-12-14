5c5d5ee1b3444e31b599b566dfa120a544a976c2
Kandrac <kandrac.jan@gmail.com>
2015-12-14 18:33:00 +0100

    Date Utils tests and fixes and test package name changed

--- app/src/main/java/xyz/kandrac/library/utils/DateUtils.java
+++ app/src/main/java/xyz/kandrac/library/utils/DateUtils.java
@@ -1,14 +1,17 @@
 package xyz.kandrac.library.utils;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by kandrac on 13/12/15.
  */
 public class DateUtils {
 
-    public static SimpleDateFormat dateFormat = new SimpleDateFormat("dd. MM. yyyy", Locale.getDefault());
+    public static SimpleDateFormat dateFormat = new SimpleDateFormat("d. M. yyyy", Locale.getDefault());
+    public static SimpleDateFormat fullDateFormat = new SimpleDateFormat("d. M. yyyy H:mm:ss", Locale.getDefault());
 
     public static long getTodayZeroTime() {
         return getZeroTime(System.currentTimeMillis());
@@ -19,10 +22,18 @@ public class DateUtils {
     }
 
     public static long getZeroTime(long timeInDay) {
-        return timeInDay - timeInDay % (1000 * 60 * 60 * 24);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(timeInDay);
+
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        return calendar.getTimeInMillis();
     }
 
     public static long getHourTime(long timeInDay, int hour) {
-        return getZeroTime(timeInDay) + 1000 * 60 * 60 * hour;
+        return getZeroTime(timeInDay) + TimeUnit.HOURS.toMillis(hour);
     }
 }
