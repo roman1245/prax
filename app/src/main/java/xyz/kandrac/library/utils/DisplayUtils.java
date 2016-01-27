f69935af3b28bdc465e1649c6b1d269a63bfeca6
Kandrac <kandrac.jan@gmail.com>
2016-01-27 11:44:59 +0100

    Resize image fix
    On some devices images was automatically rotated because of
    file EXIF definition.

--- app/src/main/java/xyz/kandrac/library/utils/DisplayUtils.java
+++ app/src/main/java/xyz/kandrac/library/utils/DisplayUtils.java
@@ -3,6 +3,8 @@ package xyz.kandrac.library.utils;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.util.DisplayMetrics;
 import android.widget.ImageView;
 
@@ -10,6 +12,7 @@ import com.squareup.picasso.Picasso;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 import xyz.kandrac.library.R;
 
@@ -44,6 +47,14 @@ public final class DisplayUtils {
 
     public static void resizeImageFile(File imageFile, int width, int quality) {
 
+        int orientation = ExifInterface.ORIENTATION_NORMAL;
+        try {
+            ExifInterface exif = new ExifInterface(imageFile.getAbsolutePath());
+            orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+        } catch (IOException ex) {
+            LogUtils.d(LOG_TAG, "Error getting EXIF from file. Assuming orientation is normal");
+        }
+
         LogUtils.d(LOG_TAG, "Resizing " + imageFile.getName() + " with size of " + imageFile.length() / 1000 + "kB");
 
         Bitmap b = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
@@ -54,9 +65,10 @@ public final class DisplayUtils {
         }
 
         float ratio = (float) b.getWidth() / (float) b.getHeight();
-        Bitmap out = Bitmap.createScaledBitmap(b, width, (int) ((float) width / ratio), false);
+        Bitmap out = getResizedBitmap(b, width, (int) ((float) width / ratio), orientation);
 
         File file = new File(imageFile.getParent(), "temp.jpg");
+
         FileOutputStream fOut;
 
         try {
@@ -82,5 +94,49 @@ public final class DisplayUtils {
                 LogUtils.d(LOG_TAG, "Unable to delete resized image");
             }
         }
+    }
+
+    /**
+     * Get properly resized bitmap based on original bitmap, requested sizes and EXIF orientation
+     *
+     * @param original    original bitmap
+     * @param newWidth    width of result bitmap
+     * @param newHeight   height of result bitmap
+     * @param orientation from EXIF details
+     * @return resized bitmap
+     */
+    public static Bitmap getResizedBitmap(Bitmap original, int newWidth, int newHeight, int orientation) {
+        int width = original.getWidth();
+        int height = original.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        Matrix matrix = new Matrix();
+        float rotate = 0;
+
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_NORMAL:
+                LogUtils.d(LOG_TAG, "Normal orientation");
+                rotate = 0;
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                LogUtils.d(LOG_TAG, "Rotating by 180");
+                rotate = 180;
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                LogUtils.d(LOG_TAG, "Rotating by 90");
+                rotate = 90;
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                LogUtils.d(LOG_TAG, "Rotating by 270");
+                rotate = 270;
+                break;
+        }
+
+        matrix.postRotate(rotate);
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        Bitmap resizedBitmap = Bitmap.createBitmap(original, 0, 0, width, height, matrix, false);
+        original.recycle();
+        return resizedBitmap;
     }
 }
