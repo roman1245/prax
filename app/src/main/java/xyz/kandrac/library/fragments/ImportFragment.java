0502bb2e924c1848a244af039fb7207371ac259b
Kandrac <kandrac.jan@gmail.com>
2016-02-22 16:49:30 +0100

    Sample import
    - not working with drive file chooser
    - output only into console and in strict order

--- app/src/main/java/xyz/kandrac/library/fragments/ImportFragment.java
+++ app/src/main/java/xyz/kandrac/library/fragments/ImportFragment.java
@@ -1,13 +1,24 @@
 package xyz.kandrac.library.fragments;
 
+import android.Manifest;
 import android.app.Fragment;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import java.io.File;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import xyz.kandrac.library.R;
+import xyz.kandrac.library.utils.BackupUtils;
 
 /**
  * Actual screen for importing data from CSV files into database.
@@ -16,9 +27,83 @@ import xyz.kandrac.library.R;
  */
 public class ImportFragment extends Fragment {
 
+    private static final int CHOOSE_FILE_REQUEST = 100;
+    private static final int CHOOSE_FILE_PERMISSION = 200;
+
+    @OnClick(R.id.import_import)
+    public void importCsv(View view) {
+        int permission = ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            invokeChooser();
+        } else {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, CHOOSE_FILE_PERMISSION);
+        }
+    }
+
+    private void invokeChooser() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("text/csv");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        // special intent for Samsung file manager
+        Intent sIntent = new Intent("com.sec.android.app.myfiles.PICK_DATA");
+        // if you want any file type, you can skip next line
+        sIntent.putExtra("CONTENT_TYPE", "text/csv");
+        sIntent.addCategory(Intent.CATEGORY_DEFAULT);
+
+        Intent chooserIntent;
+
+        if (getActivity().getPackageManager().resolveActivity(sIntent, 0) != null) {
+            // it is device with samsung file manager
+            chooserIntent = Intent.createChooser(sIntent, "Open file");
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{intent});
+        } else {
+            chooserIntent = Intent.createChooser(intent, "Open file");
+        }
+
+        try {
+            startActivityForResult(chooserIntent, CHOOSE_FILE_REQUEST);
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(getActivity().getApplicationContext(), "No suitable File Manager was found.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_import, container, false);
+        View result = inflater.inflate(R.layout.fragment_import, container, false);
+        ButterKnife.bind(this, result);
+        return result;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case CHOOSE_FILE_REQUEST:
+                File file = new File(data.getData().getPath());
+                if (file.exists()) {
+                    BackupUtils.importCsv(getActivity(), file);
+                } else {
+                    Toast.makeText(getActivity(), "error : " + data.getData().getPath(), Toast.LENGTH_LONG).show();
+                }
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case CHOOSE_FILE_PERMISSION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    invokeChooser();
+                }
+                break;
+            }
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
