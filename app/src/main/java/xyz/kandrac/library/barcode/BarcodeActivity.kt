7f33bb6f7eef2330517f8b5205aa3e2e50c571b0
Jan Kandrac <jan@kandrac.xyz>
2019-11-02 14:54:04 +0100

    AndroidX + ML Kit - barcode scanner

---
+++ app/src/main/java/xyz/kandrac/library/barcode/BarcodeActivity.kt
@@ -0,0 +1,165 @@
+package xyz.kandrac.library.barcode
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Matrix
+import android.os.Bundle
+import android.util.Size
+import android.view.Surface
+import android.view.TextureView
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata
+import xyz.kandrac.library.R
+import java.util.concurrent.Executors
+
+class BarcodeActivity : AppCompatActivity() {
+
+    private val executor = Executors.newSingleThreadExecutor()
+    private lateinit var viewFinder: TextureView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.barcode_scan)
+
+        viewFinder = findViewById(R.id.view_finder)
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            viewFinder.post { startCamera() }
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+            updateTransform()
+        }
+
+    }
+
+    private fun startCamera() {
+        val previewConfig = PreviewConfig.Builder().apply {
+            setTargetResolution(Size(480, 360))
+        }.build()
+
+        val preview = Preview(previewConfig)
+
+        preview.setOnPreviewOutputUpdateListener {
+            val parent = viewFinder.parent as ViewGroup
+            parent.removeView(viewFinder)
+            parent.addView(viewFinder, 0)
+
+            viewFinder.surfaceTexture = it.surfaceTexture
+            updateTransform()
+        }
+
+        val analyzerConfig = ImageAnalysisConfig.Builder().apply {
+            setImageReaderMode(ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE)
+        }.build()
+
+        val analyzerUseCase = ImageAnalysis(analyzerConfig).apply {
+            setAnalyzer(executor, YourImageAnalyzer {
+                setResult(Activity.RESULT_OK, Intent().apply { putExtra(BARCODE_TEXT, it.rawValue!!) })
+                finish()
+            })
+        }
+
+        CameraX.bindToLifecycle(this, preview, analyzerUseCase)
+
+    }
+
+    private fun updateTransform() {
+        val matrix = Matrix()
+
+        // Compute the center of the view finder
+        val centerX = viewFinder.width / 2f
+        val centerY = viewFinder.height / 2f
+
+        // Correct preview output to account for display rotation
+        val rotationDegrees = when (viewFinder.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+
+        // Finally, apply transformations to our TextureView
+        viewFinder.setTransform(matrix)
+    }
+
+    /**
+     * Process result from permission request dialog box, has the request
+     * been granted? If yes, start Camera. Otherwise display a toast
+     */
+    override fun onRequestPermissionsResult(
+            requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                viewFinder.post { startCamera() }
+            } else {
+
+                // TODO: localize
+                Toast.makeText(this,
+                        "Permissions not granted by the user.",
+                        Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    /**
+     * Check if all permission specified in the manifest have been granted
+     */
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+                baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+
+    companion object {
+
+        // This is an arbitrary number we are using to keep track of the permission
+        // request. Where an app has multiple context for requesting permission,
+        // this can help differentiate the different contexts.
+        private const val REQUEST_CODE_PERMISSIONS = 10
+
+        // This is an array of all the permission specified in the manifest.
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
+        const val BARCODE_TEXT = "result_barcode_text"
+    }
+}
+
+private class YourImageAnalyzer(val processor: (FirebaseVisionBarcode) -> Unit) : ImageAnalysis.Analyzer {
+    private fun degreesToFirebaseRotation(degrees: Int): Int = when (degrees) {
+        0 -> FirebaseVisionImageMetadata.ROTATION_0
+        90 -> FirebaseVisionImageMetadata.ROTATION_90
+        180 -> FirebaseVisionImageMetadata.ROTATION_180
+        270 -> FirebaseVisionImageMetadata.ROTATION_270
+        else -> throw Exception("Rotation must be 0, 90, 180, or 270.")
+    }
+
+    override fun analyze(imageProxy: ImageProxy?, degrees: Int) {
+        val mediaImage = imageProxy?.image
+        val imageRotation = degreesToFirebaseRotation(degrees)
+        if (mediaImage != null) {
+            val image = FirebaseVisionImage.fromMediaImage(mediaImage, imageRotation)
+            val detector = FirebaseVision.getInstance().visionBarcodeDetector
+            val result = detector.detectInImage(image)
+                    .addOnSuccessListener { barcodes -> if (barcodes != null && barcodes.size > 0) processor(barcodes[0]) }
+                    .addOnFailureListener {}
+        }
+    }
+}
